3206d3619864bc8ef4885a3d8db2467092a86fae
Mark Arneman <mark@arneman.me>
2019-04-05 15:49:14 -0400

    Obstacle spawner, Obstacle Approach, Project organization

---
+++ Assets/_2btn/Scripts/ObstacleApproach.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleApproach : MonoBehaviour {
+
+    [SerializeField]
+    private float objectSpeed = 10.0f;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate () {
+        rb.velocity -= transform.forward * objectSpeed * Time.deltaTime;
+        Debug.Log("Velocity: " + rb.velocity);
+    }
+
+    private void OnBecameInvisible () {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_2btn/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private float spawnTime = 3.0f;
+
+    [SerializeField]
+    private GameObject obstaclePrefab;
+
+    private bool hasSpawned = false;
+
+	// Use this for initialization
+	void Start () {        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasSpawned) {
+            StartCoroutine(SpawnObstacle());
+        }
+	}
+
+    IEnumerator SpawnObstacle() {
+        hasSpawned = true;
+
+        // TODO: Spawn obstacles on random walls
+
+        float xPos = Random.Range(-6.0f, 6.0f);
+        Instantiate(obstaclePrefab, new Vector3(xPos, 0, 88.0f), Quaternion.identity); // 88 magic number because my wife loves 8s
+
+        yield return new WaitForSeconds(spawnTime);
+
+        hasSpawned = false;
+    }
+}
