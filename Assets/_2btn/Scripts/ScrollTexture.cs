83d46cceded6ee3684657d24a221979217ac4ead
Rensei <32361344+17068274@users.noreply.github.com>
2019-04-09 13:55:28 +0100

    Deleted not used MK glow assets + other fixes
    - Changed players collision box size to 0.9x0.9x.09
    - Alligned obstacles with even scale to the grid horizontaly
    - Changed how obstacles move
    - Synced obstacle speed to scroll speed
    - Cleaned-up scrollTexture script a bit

--- Assets/_2btn/Scripts/ScrollTexture.cs
+++ Assets/_2btn/Scripts/ScrollTexture.cs
@@ -5,32 +5,37 @@ using UnityEngine;
 public class ScrollTexture : MonoBehaviour {
 
     // Scroll main texture based on time
-    public float scrollSpeed = 0.5f;
+    public float scrollSpeed = 1f;
     Renderer rend;
 
-    public bool scrollWalls = false;
-    public bool rotateEnd = false;
+    [SerializeField]
+    private bool scrollWalls = false;
+    [SerializeField]
+    private bool rotateEnd = false;
+
+    private float offset;
+
     void Start () {
         rend = GetComponent<Renderer> ();
     }
-    float offset;
     void Update () {
+
+        offset += Time.deltaTime * scrollSpeed;
+
         //Ground and ceiling
         if (!scrollWalls) {
-            offset = Time.time * scrollSpeed;
+
             rend.material.SetTextureOffset ("_MainTex", new Vector2 (0.3f, -offset));
             rend.material.SetTextureOffset("_MKGlowTex", new Vector2(0.3f, -offset));
         }
         //Left and right wall
         else
         {
-            offset = Time.time * scrollSpeed;
             rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0.3f));
             rend.material.SetTextureOffset("_MKGlowTex", new Vector2(offset, 0.3f));
         }
         if (rotateEnd)
         {
-            offset = Time.time * scrollSpeed;
             rend.material.SetTextureOffset("_MainTex", new Vector2(offset, -offset));
             rend.material.SetTextureOffset("_MKGlowTex", new Vector2(offset, -offset));
         }
